674d2102b0796fbda3a12484b5d2bb6ef2cd6887
ivanstark <i@stark11.ru>
2024-10-17 22:52:34 +0300

    logging and fixing

--- src/planning/planner.py
+++ src/planning/planner.py
@@ -1,5 +1,6 @@
 import ast
 import logging
+import os
 import re
 import sys
 from typing import Tuple, Type
@@ -11,8 +12,13 @@ from src.primitives.checks import *
 from src.environment.code_parser import find_most_function_calls
 from src.planning.llm_caller import LLMCaller
 
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+log_dir = '../../test/logs/'
+os.makedirs(log_dir, exist_ok=True)
+
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
 logger = logging.getLogger('MAIN_PLANNING_LOGGER')
+
 
 TASKS = [
     'Collect wood',
@@ -79,7 +85,7 @@ class Planner:
 
 
     def get_plan(self, env, num_tries_replanning=3):
-        logger.info('Generating plan...')
+        logger.debug('Generating plan...')
         self.llm_caller.reset()
 
         # создаем среду, чтобы прокинуть в промпт obs
@@ -87,30 +93,30 @@ class Planner:
         starting_prompt = self.llm_caller.get_full_starting_prompt(env)
 
         # получаем первичный код
-        logger.info('Calling LLM...')
+        logger.debug('Calling LLM...')
         response = self.llm_caller.call_llm(starting_prompt)
-        logger.info('validating code...')
+        logger.debug('validating code...')
         code, code_is_valid = self.parse_code_from_response(response)
 
         replan_prompt = ''
         if not code_is_valid:
             # если первичный код сгенерирован с ошибкой
-            logger.info(f'Первичный код сгенерирован с ошибкой: {code}')
+            logger.debug(f'Первичный код сгенерирован с ошибкой: {code}')
             replan_prompt = self.llm_caller.get_error_prompt(code)
         else:
             # играем первую траекторию
-            logger.info(f'Играем первый промпт: {code}\n\n')
+            logger.debug(f'Играем первый промпт: {code}\n\n')
             play_result = self.play_code(code, env)
             if play_result is not None:
                 # если первичный код падает с ошибкой
-                logger.info(f'Первичный код падает с ошибкой: {play_result}')
+                logger.debug(f'Первичный код падает с ошибкой: {play_result}')
                 replan_prompt = self.llm_caller.get_error_prompt(play_result)
             elif not self.task_checker(env):
                 # если первичный код не выполняет задачу
-                logger.info('Первичный код не выполняет задачу')
+                logger.debug('Первичный код не выполняет задачу')
                 replan_prompt = self.llm_caller.get_debug_prompt()
             else:
-                logger.info('Первичный код выполняет задачу')
+                logger.debug('Первичный код выполняет задачу')
                 return code  # код сразу сгенерирован удачно
 
 
@@ -119,60 +125,71 @@ class Planner:
             env.reset(self.main_seed)
 
             # получаем новый код
-            logger.info(f'Попытка {planning_iteration + 1} из {num_tries_replanning}')
-            logger.info(f'Перепланнируем код...\n')
+            logger.debug(f'Попытка {planning_iteration + 1} из {num_tries_replanning}')
+            logger.debug(f'Перепланнируем код...\n')
             response = self.llm_caller.call_llm(replan_prompt)
             code, code_is_valid = self.parse_code_from_response(response)
 
             # если код невалидный
             if not code_is_valid:
                 # если первичный код сгенерирован с ошибкой
-                logger.info(f'Первичный код сгенерирован с ошибкой: {code}')
+                logger.debug(f'Первичный код сгенерирован с ошибкой: {code}')
                 replan_prompt = self.llm_caller.get_error_prompt(code)
                 continue
             else:
                 # играем траекторию
-                logger.info(f'Играем код: {code}\n\n')
+                logger.debug(f'Играем код: {code}\n\n')
                 play_result = self.play_code(code, env)
                 if play_result is not None:
                     # если код падает с ошибкой
-                    logger.info(f'Код падает с ошибкой: {play_result}')
+                    logger.debug(f'Код падает с ошибкой: {play_result}')
                     replan_prompt = self.llm_caller.get_error_prompt(play_result)
                 elif not self.task_checker(env):
                     # если код не выполняет задачу
-                    logger.info('Код не выполняет задачу')
+                    logger.debug('Код не выполняет задачу')
                     replan_prompt = self.llm_caller.get_debug_prompt()
                 else:
-                    logger.info('Код выполняет задачу')
+                    logger.debug('Код выполняет задачу')
                     return code  # код сгенерирован удачно
 
         print('число попыток превышено.')
         return None
 
     def estimate_success_rate(self, env, code, num_iter=10):
-        logger.info('Estimating success rate...')
+        logger.debug('Estimating success rate...')
         results = []
         for iteration in range(num_iter):
             seed = self.main_seed + iteration
             env.reset(seed)
 
-            logger.info(f'Estimating success rate: {iteration + 1} из {num_iter}')
+            logger.debug(f'Estimating success rate: {iteration + 1} из {num_iter}')
             result = self.play_code(code, env)
             if result is None:
-                logger.info('Failed to play code')
+                logger.debug('Failed to play code')
                 results.append(self.task_checker(env))
             else:
-                logger.info(f'result: {result}')
+                logger.debug(f'result: {result}')
                 results.append(0)
         return (1. * sum(results)) / len(results)
 
 
 if __name__ == '__main__':
     env = make_craftax_env_from_name("Craftax-Symbolic-v1", auto_reset=False)
-    env = SaveStateWrapper(env, seed=42, log_dir='../../logs/')
+    env = SaveStateWrapper(env, seed=42, log_dir=log_dir)
     obs, state = env.reset(seed=42)
 
-    planner = Planner(task='Make wood pickaxe',
+    logger.setLevel(logging.DEBUG)
+
+    log_filepath = os.path.join(log_dir, 'planner.log')
+
+    if os.path.exists(log_filepath):
+        os.remove(log_filepath)
+
+    file_handler = logging.FileHandler(log_filepath)
+    file_handler.setLevel(logging.INFO)
+    logger.addHandler(file_handler)
+
+    planner = Planner(task='Collect wood',
                       api_key='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')
     code = planner.get_plan(env)
     if code is not None:
