0e39a950ba5b95d5a9477e2a15dabebeed13be37
ivanstark <i@stark11.ru>
2024-10-17 21:00:01 +0300

    add step_counting

--- src/environment/wrapper.py
+++ src/environment/wrapper.py
@@ -9,20 +9,24 @@ class SaveStateWrapper(Wrapper):
         self.rng = jax.random.PRNGKey(self.seed)
         obs, state = self.env.reset(self.rng)
         self.saved_state = state
+        self.action_count = 0
         return obs, state
 
     def step(self, action):
         self.rng, subkey = jax.random.split(self.rng)
         obs, state, reward, done, info = self.env.step(subkey, self.saved_state, action.value, self.env.default_params)
         self.saved_state = state
+        self.action_count += 1
         return obs, state, reward, done, info
 
-    def __init__(self, env, seed, log_dir):
+    def __init__(self, env, seed, log_dir, max_steps=400):
         super().__init__(env)
         self.env = env
         self.seed = seed
         # self.log_dir = 'logs/actions.csv'
         self.log_dir = log_dir
+        self.action_count: int = 0
+        self.max_steps: int = max_steps
 
 # class SaveStateWrapper(gym.Wrapper):
 #     def __init__(self, env, seed=0xBAD_5EED, log_dir: str = '../../logs/'):
