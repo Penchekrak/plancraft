225ba7cd7bb2dad754d3d3e27454ff1d7176a249
ivanstark <i@stark11.ru>
2024-10-17 15:54:14 +0300

    added smart PLACE_FURNACE Action added more logging to simple actions

--- src/primitives/simple_actions.py
+++ src/primitives/simple_actions.py
@@ -1,6 +1,20 @@
-from craftax.craftax.constants import Action
-from src.environment.executor import executor
+import logging
+from turtledemo.sorting_animate import Block
 
+from craftax.craftax.constants import Action, BlockType, DIRECTIONS
+from environment.executor import check_nearby_table, check_nearby_furnace
+from primitives.checks import *
+from src.environment.executor import executor
+from src.graphs.move_to_node_smart import DIRECTIONS_TO_ACTIONS, NEED_DIG, NEED_CHOP
+
+logger = logging.getLogger()
+
+# UTILS
+def act_MOVE_DIRECTION(env, direction):
+    executor(env, [DIRECTIONS_TO_ACTIONS[direction]])
+
+
+# Simple Actions
 
 def act_DO(env):
     action = Action.DO
@@ -15,56 +29,133 @@ def act_PLACE_STONE(env):
 def act_PLACE_TABLE(env):
     action = Action.PLACE_TABLE
     executor(env, [action])
+    if check_forward_block(env) == BlockType.CRAFTING_TABLE:
+        logger.info('Placed table')
+    else:
+        logger.info('Could not place table')
 
 
 def act_PLACE_FURNACE(env):
-    action = Action.PLACE_FURNACE
-    executor(env, [action])
+    # if you can just place it
+    if check_forward_block(env) in [BlockType.PATH, BlockType.GRASS, BlockType.SAND]:
+        action = Action.PLACE_FURNACE
+        executor(env, [action])
+        logger.info('Placed furnace')
+        return
+
+    # if there already is a furnace
+    if check_nearby_furnace(env):
+        logger.info('Furnace already exists nearby')
+        return
+
+    # if crafting_table is in front of you
+    if check_forward_block(env) == BlockType.CRAFTING_TABLE:
+        for direction in DIRECTIONS[1: 5]:
+            pos = env.saved_state.player_position + direction
+            block = BlockType(env.saved_state.map[env.saved_state.player_level][pos[0], pos[1]])
+            if block == BlockType.CRAFTING_TABLE:
+                continue
+
+            if block in NEED_DIG + NEED_CHOP:
+                act_MOVE_DIRECTION(env, direction)
+                act_DO(env)
+                act_PLACE_FURNACE(env)
+                return
+
+            if block in [BlockType.PATH, BlockType.GRASS, BlockType.SAND]:
+                opposite_direction = -direction
+                opposite_pos = env.saved_state.player_position + opposite_direction
+                opposite_block = BlockType(
+                    env.saved_state.map[env.saved_state.player_level][opposite_pos[0], opposite_pos[1]]
+                )
+                # if you can rotate
+                if opposite_block in [BlockType.PATH, BlockType.GRASS, BlockType.SAND]:
+                    act_MOVE_DIRECTION(env, opposite_direction)
+                    act_MOVE_DIRECTION(env, direction)
+                    act_PLACE_FURNACE(env)
+                    return
+                else:
+                    continue
+    logger.info('Could not place furnace!')
 
 
 def act_PLACE_PLANT(env):
     action = Action.PLACE_PLANT
+    logger.info('Placing plant...')
     executor(env, [action])
+
 
 
 def act_MAKE_WOOD_PICKAXE(env):
     action = Action.MAKE_WOOD_PICKAXE
     executor(env, [action])
+    if check_inventory_wood_pickaxe(env):
+        logger.info('Made wood pickaxe')
+    else:
+        logger.info('Could not make wood pickaxe')
 
 
 def act_MAKE_STONE_PICKAXE(env):
     action = Action.MAKE_STONE_PICKAXE
     executor(env, [action])
+    if check_inventory_stone_pickaxe(env):
+        logger.info('Made stone pickaxe')
+    else:
+        logger.info('Could not make stone pickaxe')
 
 
 def act_MAKE_IRON_PICKAXE(env):
     action = Action.MAKE_IRON_PICKAXE
     executor(env, [action])
+    if check_inventory_iron_pickaxe(env):
+        logger.info('Made iron pickaxe')
+    else:
+        logger.info('Could not make iron pickaxe')
 
 
 def act_MAKE_WOOD_SWORD(env):
     action = Action.MAKE_WOOD_SWORD
     executor(env, [action])
+    if check_inventory_wood_sword(env):
+        logger.info('Made wood sword')
+    else:
+        logger.info('Could not make wood sword')
 
 
 def act_MAKE_STONE_SWORD(env):
     action = Action.MAKE_STONE_SWORD
     executor(env, [action])
+    if check_inventory_stone_sword(env):
+        logger.info('Made stone sword')
+    else:
+        logger.info('Could not make stone sword')
 
 
 def act_MAKE_IRON_SWORD(env):
     action = Action.MAKE_IRON_SWORD
     executor(env, [action])
+    if check_inventory_iron_sword(env):
+        logger.info('Made iron sword')
+    else:
+        logger.info('Could not make iron sword')
 
 
 def act_MAKE_DIAMOND_PICKAXE(env):
     action = Action.MAKE_DIAMOND_PICKAXE
     executor(env, [action])
+    if check_inventory_diamond_pickaxe(env):
+        logger.info('Made diamond pickaxe')
+    else:
+        logger.info('Could not make diamond pickaxe')
 
 
 def act_MAKE_DIAMOND_SWORD(env):
     action = Action.MAKE_DIAMOND_SWORD
     executor(env, [action])
+    if check_inventory_diamond_sword(env):
+        logger.info('Made diamond sword')
+    else:
+        logger.info('Could not make diamond sword')
 
 
 def act_MAKE_IRON_ARMOUR(env):
